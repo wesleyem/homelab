a60ced2c18d217ec3994b9f58002e096eba1421a
Wes <wesleyem@pm.me>
2023-12-09 12:49:05 -0800

    clean up repeater for cron

--- oci/arm-compute-1/repeater.sh
+++ oci/arm-compute-1/repeater.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-terraform apply -auto-approve 2>> repeater-err.log 1>> repeater.log
+terraform apply \
+    -auto-approve \
+    -no-color \
+    2>> repeater-err.log \
+    1>> repeater.log
 exit
