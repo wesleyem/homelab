2c88abcac6e81f7dcb0084db73b3f6d06ef7d90d
Wes <wesleyem@pm.me>
2023-12-09 15:48:15 -0800

    update repeater script and add log rotate

--- oci/arm-compute-1/repeater.sh
+++ oci/arm-compute-1/repeater.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+# run with cron to grab available instance
+log_dir="/root/projects/homelab/oci/arm-compute-1/logs"
+error_log_filename="repeater-errlog"
+log_filename="repeaterlog"
+timestamp="Timestamp: $(date)"
+echo $timestamp >> $log_dir/$error_log_filename
+echo $timestamp >> $log_dir/$log_filename
 terraform apply \
     -auto-approve \
     -no-color \
-    2>> repeater-err.log \
-    1>> repeater.log
+    2>> $log_dir/$error_log_filename \
+    1>> $log_dir/$log_filename
 exit
