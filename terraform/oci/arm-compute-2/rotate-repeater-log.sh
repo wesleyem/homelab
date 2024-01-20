23152c50c127eb1bb701040b66d00f4bdaba79ab
Wes <wesleyem@pm.me>
2024-01-20 12:01:59 -0800

    update log rotate script

--- terraform/oci/arm-compute-2/rotate-repeater-log.sh
+++ terraform/oci/arm-compute-2/rotate-repeater-log.sh
@@ -2,7 +2,7 @@
 # use with cron to move log files
 error_log_filename="repeater-errlog"
 log_filename="repeaterlog"
-working_dir="/root/projects/homelab/oci/arm-compute-1/logs"
+working_dir="/root/projects/homelab/oci/arm-compute-2/logs"
 timestamp=$(date +%s)
 cd $working_dir
 mv $error_log_filename repeater-errlog-$timestamp
