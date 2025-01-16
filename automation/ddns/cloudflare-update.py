9dba34b402a3ca7727dd793f6a84c2062ae37f51
wesleyem <wesleyem@pm.me>
2025-01-15 21:01:41 -0800

    fix(ddns): fix env vars and remove api email

--- automation/ddns/cloudflare-update.py
+++ automation/ddns/cloudflare-update.py
@@ -5,10 +5,10 @@ from cloudflare import Cloudflare
 import os
 
 # Configuration from environment variables
-API_TOKEN = os.getenv("CLOUDFLARE_API_TOKEN")
-API_EMAIL = os.getenv("CLOUDFLARE_API_EMAIL")
-ZONE_ID = os.getenv("ZONE_ID")
-DNS_RECORD_ID = os.getenv("DNS_RECORD_ID")
+CLOUDFLARE_API_TOKEN = os.getenv("CLOUDFLARE_API_TOKEN")
+CLOUDFLARE_API_EMAIL = os.getenv("CLOUDFLARE_API_EMAIL")
+CLOUDFLARE_ZONE_ID = os.getenv("CLOUDFLARE_ZONE_ID")
+CLOUDFLARE_DNS_RECORD_ID = os.getenv("CLOUDFLARE_DNS_RECORD_ID")
 
 def get_public_ip():
     """Fetch the public IP address."""
@@ -23,13 +23,12 @@ def get_public_ip():
 def update_cloudflare_record(ip):
     """Monitor IP changes and edit Cloudflare."""
     client = Cloudflare(
-        api_email=API_EMAIL,
-        api_key=API_TOKEN
+        api_key=CLOUDFLARE_API_TOKEN
     )
     """Edit the Cloudflare DNS record."""
     try:
         # Edit the DNS record
-        client.dns.records.edit(dns_record_id=DNS_RECORD_ID, zone_id=ZONE_ID, content=ip)
+        client.dns.records.edit(dns_record_id=CLOUDFLARE_DNS_RECORD_ID, zone_id=CLOUDFLARE_ZONE_ID, content=ip)
         print(f"Edited Cloudflare DNS record to IP: {ip}")
     except Exception as e:
         print(f"Error editing Cloudflare DNS record: {e}")
